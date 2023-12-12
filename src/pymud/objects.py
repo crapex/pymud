9b0ae45f3d4918419bab12893d23eaa5e265a94a
liuyang <crapex@hotmail.com>
2023-12-12 08:44:29 +0800

    update 0.16.1.post1

--- src/pymud/objects.py
+++ src/pymud/objects.py
@@ -107,8 +107,8 @@ class CodeLine:
         if new_code[0] == "#":
             await session.handle_input_async(new_code_str)
         else:
-            await session.exec_command_async(" ".join(new_code))
-
+            #await session.exec_command_async(" ".join(new_code))
+            await session.exec_command_async(new_code_str)
 
 class CodeBlock:
     """
