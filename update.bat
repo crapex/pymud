29080249b7b84a7efd445b1530790a82670cd2b0
liuyang <crapex@hotmail.com>
2025-04-04 12:41:44 +0800

    continue for i18n

--- update.bat
+++
@@ -1,8 +0,0 @@
-copy src\pymud\*.py ..\py310\Lib\site-packages\pymud\*.py
-copy src\pymud\*.py ..\py312\Lib\site-packages\pymud\*.py
-mkdir..\py310\Lib\site-packages\pymud\lang\
-mkdir..\py312\Lib\site-packages\pymud\lang\
-copy src\pymud\lang\*.py ..\py310\Lib\site-packages\pymud\lang\*.py
-copy src\pymud\lang\*.py ..\py312\Lib\site-packages\pymud\lang\*.py
-::mkdir ..\pkuxkx\venv\Lib\site-packages\pymud\plugins\
-::copy src\pymud\plugins\*.py ..\pkuxkx\venv\Lib\site-packages\pymud\plugins\*.py
