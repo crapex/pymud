42e8349f1aef1e80d7c73fcdfffa87b59105aada
Kokomi <lian_haitao@163.com>
2025-08-22 18:47:59 +0800

    Delete src/Code directory

--- src/Code/MudApp.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-from pymud.pymud import *
-
-cfg = "pymud.cfg"
-with open(cfg, "r", encoding="utf8", errors="ignore") as fp:
-    cfg_data = json.load(fp)
-    app = PyMudApp(cfg_data)
-    app.run()
--- src/Code/script/misc/formatMudStr.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def remove_ansi_color(text):
-    if isinstance(text, str) and '\x1b' in text:
-        ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
-        return ansi_escape.sub('', text)
-    else:
-        return text
